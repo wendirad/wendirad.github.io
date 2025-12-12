054a7ea960200856c2c2f422289380b0221e4346
Wendirad Demelash <wendiradame@gmail.com>
2025-12-12 12:54:48 -0800

    feat: add info section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css'
+import ThemeToggle from './components/ThemeToggle'
+import InfoSection from './components/InfoSection'
 
 function App() {
   return (
     <>
-      
+      <ThemeToggle />
+      <InfoSection />
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{html,js,ts,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'text-light': 'rgb(68, 68, 68)',
+        'text-dark': 'rgb(230, 230, 230)',
+        'bg-light': '#DBDBDB',
+        'bg-dark': '#525252',
+        'primary-light': '#C4C4C4',
+        'primary-dark': '#696969',
+        'secondary-light': '#9E9E9E',
+        'secondary-dark': '#808080',
+        'tertiary-light': '#71797E',
+        'tertiary-dark': '#E5E4E2',
+      },
+      fontFamily: {
+        sans: ['Signika', 'Gilroy', 'sans-serif'],
+        display: ['Gilroy', 'Signika', 'sans-serif'],
+        body: ['Signika', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
